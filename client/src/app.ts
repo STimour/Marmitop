641b6f6292c6cff4391c9b6de1cff57bff921c8d
STimour <st.sayfoutdinov@icloud.com>
2023-09-18 09:00:28 +0200

    ajout de fonctionnalités: PUT, DELETE

--- client/src/app.ts
+++ client/src/app.ts
@@ -1,8 +1,8 @@
-const apiUrl = "http://localhost:1024/"
+const apiUrlGetPost = "http://localhost:1024/api/recettes"
 
 async function fetchData() {
     try{
-        const response = await fetch(`${apiUrl}`)
+        const response = await fetch(`${apiUrlGetPost}`)
         if(!response.ok){
             throw new Error("Get n'a pas marché")
         }
@@ -13,10 +13,14 @@ async function fetchData() {
         if(outputSection){
             outputSection.innerHTML = '';
             data.forEach((recette: { 
+                                        id: number,
                                         titre: string, 
                                         image_link: string, 
                                         time_duration: string, 
-                                        note: string}) => {
+                                        note: string,
+                                        posting_date: string}) => {
+
+                const postingDate = new Date(recette.posting_date)
                 outputSection = document.getElementById('output_section')
                                         
                 const recetteSection = document.createElement('div') as HTMLDivElement
@@ -33,10 +37,114 @@ async function fetchData() {
                 const titreH1 = document.createElement("h1") 
                 const timeP =  document.createElement("p") as HTMLParagraphElement
                 const noteP =  document.createElement("p") as HTMLParagraphElement
-                                        
+                const ajoutP =  document.createElement("p") as HTMLParagraphElement
+
+                const btnDelet = document.createElement("button") as HTMLButtonElement
+                      btnDelet.textContent = "Supprimer"
+                      btnDelet.setAttribute("class", "btnDelet p-10")
+                      btnDelet.addEventListener("click", async () => {
+                          const confirmation = confirm("Êtes-vous sûr de la suppression?")
+                          if (confirmation){
+                            try{
+                              const response = await fetch(`http://localhost:1024/api/recettes/${recette.id}`, {
+                                method: "DELETE"
+                              })
+                              if(!response.ok){
+                                throw new Error("echec")
+                              }
+                              const info = await response.json()
+
+                              alert(info.message)
+                              fetchData()
+                            }catch(e){
+                              console.log(e)
+                            }
+                          }
+                      })                    
+
+                const btnModif = document.createElement("button") as HTMLButtonElement
+                      btnModif.textContent = "Modifier"   
+                      btnModif.setAttribute("class", "btnModif p-10")     
+                      btnModif.addEventListener("click", () => {
+                  
+                        recetteImageDiv.remove()
+                        recetteTextDiv.remove()
+                        
+                        const input_form_sec_up = document.createElement("div") as HTMLDivElement
+                            input_form_sec_up.setAttribute("class", "flex-column w-900 m-auto text_center border")
+                    
+
+                        const input_form_sec_bottom = document.createElement("div") as HTMLDivElement
+
+                        const formulaireModif =  document.createElement("form") as HTMLFormElement
+                        formulaireModif.setAttribute("class", "m-50")
+
+
+                        
+                        recetteSection.appendChild(formulaireModif);
+
+                        const titreInput = document.createElement("input") as HTMLInputElement
+                        titreInput.type = "text"
+                        
+                        titreInput.value = recette.titre
+
+                        const imageLinkInput = document.createElement("input") as HTMLInputElement
+                        imageLinkInput.type = "text";
+                        imageLinkInput.value = recette.image_link;
+
+                        const durationInput = document.createElement("input") as HTMLInputElement
+                        durationInput.type = "text"
+                        durationInput.value = recette.time_duration
+
+                        const noteInput = document.createElement("input") as HTMLInputElement
+                        noteInput.type = "text"
+                        noteInput.value = recette.note
+
+                        formulaireModif.appendChild(titreInput)
+                        formulaireModif.appendChild(imageLinkInput)
+                        formulaireModif.appendChild(durationInput)
+                        formulaireModif.appendChild(noteInput)
+
+                        const submitBtn = document.createElement("button") as HTMLButtonElement;
+                        submitBtn.type = "submit";
+                        submitBtn.textContent = "Modifier";
+                        formulaireModif.appendChild(submitBtn);
+
+                        formulaireModif.addEventListener("submit", async (e) => {
+                          e.preventDefault()
+                          
+                          const newTitre = titreInput.value
+                          const newImageLink = imageLinkInput.value
+                          const newDuration = durationInput.value
+                          const newNote = noteInput.value
+                          const newPostingDate = new Date()
+
+                          try{
+                            const response = await fetch(`http://localhost:1024/api/recettes/${recette.id}`, {
+                              method: "PUT",
+                              headers: {
+                                "Content-Type": "application/json"
+                              },
+                              body: JSON.stringify({titre: newTitre, imageRecipe: newImageLink, time_duration: newDuration, note: newNote, posting_date: newPostingDate.toISOString()
+                              })
+                            })
+                            if(!response.ok){
+                              throw new Error("echec put")
+                            }
+
+                            fetchData()
+                          } catch(e) {
+                            console.log(e);
+                            
+                          }
+                        
+                        })
+                      })
+
                 titreH1.textContent = recette.titre
-                timeP.textContent = recette.time_duration
+                timeP.textContent = recette.time_duration + " min."
                 noteP.textContent = recette.note
+                ajoutP.textContent = "Posté le " + postingDate.toLocaleDateString()
                 imageRecipe.setAttribute("src", recette.image_link)
                 imageRecipe.setAttribute("class", "img")
                 outputSection?.appendChild(recetteSection)
@@ -45,10 +153,14 @@ async function fetchData() {
                 recetteImageDiv.appendChild(imageRecipe)
                 recetteTextDiv.appendChild(titreH1)
                 recetteTextDiv.appendChild(timeP)
+                recetteTextDiv.appendChild(ajoutP)
                 recetteTextDiv.appendChild(noteP) 
+                recetteTextDiv.appendChild(btnModif)
+                recetteTextDiv.appendChild(btnDelet)
             })
+
         }else{
-            console.error("L'élément 'output_section' n'a pas été trouvé.");
+            console.error("L'élément 'output_section' n'a pas été trouvé.")
         }
     }catch(e){
         console.error('Erreur lors de la requête GET :', e)
@@ -60,11 +172,11 @@ window.addEventListener('load', () => {
     fetchData();
   });
 
-const formulaire = document.querySelector("form")
+const formulaire = document.getElementById("formReciepInput")
 
 if (formulaire) {
   formulaire.addEventListener("submit", async (e) => {
-    e.preventDefault()
+   e.preventDefault()
 
     const titreElement = document.getElementById("title") as HTMLInputElement
     const imageLinkElement = document.getElementById("image_link") as HTMLInputElement
@@ -83,7 +195,7 @@ if (formulaire) {
       const note = noteElement.value
 
       try {
-        const response = await fetch("http://localhost:1024/ajouter-recette", {
+        const response = await fetch(`${apiUrlGetPost}`, {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
@@ -107,4 +219,3 @@ if (formulaire) {
     }
   })
 }
-//fetchData()
--- src/server/index.ts
+++ src/server/index.ts
@@ -4,8 +4,11 @@ import dotenv, { configDotenv } from "dotenv"
 import cors from 'cors'
 import ViteExpress from "vite-express"
 import pg from "pg"
+import { log } from "console"
 
 
+const apiUrlGetPost = "/api/recettes"
+const apiUrlIdDeletIdPut = "/api/recettes/:id"
 
 
 
@@ -15,8 +18,8 @@ const app = express()
 app.use(express.json())
 
 app.use(cors({
-        origin: 'http://localhost:5173',
-        methods: ['GET','POST']
+        origin: '*',
+        methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
     }))
 
 
@@ -30,7 +33,6 @@ const client = new Client({
 }) 
 
 
-connect()
 async function connect(){
     try{
         await client.connect()
@@ -40,13 +42,14 @@ async function connect(){
         
     }
 }
+connect()
 
 const portString = process.env.PORT
 
 if(typeof portString === "string" && !isNaN(parseInt(portString))){
     const PORT = parseInt(portString)
 
-    app.get("/", async (_, res) => {
+    app.get(`${apiUrlGetPost}`, async (_, res) => {
         try{ 
             const resultat = await client.query("select * from recette")
             res.json(resultat.rows)
@@ -55,7 +58,7 @@ if(typeof portString === "string" && !isNaN(parseInt(portString))){
         }
     })
 
-    app.post("/ajouter-recette", async (req, res) => {
+    app.post(`${apiUrlGetPost}`, async (req, res) => {
         try {
           const { titre, image_link, time_duration, note } = req.body;
           
@@ -69,8 +72,58 @@ if(typeof portString === "string" && !isNaN(parseInt(portString))){
           console.error("Erreur lors de l'ajout de la recette :", e);
           res.status(500).json({ error: "Erreur lors de l'ajout de la recette" });
         }
-      });
+    });
       
+    app.delete(`${apiUrlIdDeletIdPut}`, async (req, res) => {
+        const recetteId = req.params.id
+        try{
+            const resultat = await client.query("DELETE FROM recette WHERE id = $1",
+            [recetteId]
+            )
+            if(resultat.rowCount === 0){
+                return res.status(404).json({message: "Recette n'existe pas dans la base de donnée"})
+            }
+            res.json({ message: "Recette supprimée avec succès" })
+        }catch(e){
+            console.log(`olala : ${e}`);
+            res.status(500).json({ error: "Erreur lors de la suppression de la recette" });
+        }
+    })
+
+    app.put(`${apiUrlIdDeletIdPut}`, async (req, res) => {
+        const recetteId = req.params.id
+        const {titre, image_link, time_duration, note, posting_date} = req.body
+        try {
+            const recetteActuelle = await client.query("SELECT * FROM recette WHERE id = $1", [recetteId]);
+    
+            if (recetteActuelle.rowCount === 0) {
+                return res.status(404).json({ message: "Recette non trouvée" });
+            }
+    
+            const recetteCourante = recetteActuelle.rows[0];
+    
+            // Fusionnez les valeurs de la demande avec les valeurs actuelles de la recette
+            const { titre, image_link, time_duration, note, posting_date } = req.body;
+            const nouvelleRecette = {
+                titre: titre || recetteCourante.titre,
+                image_link: image_link || recetteCourante.image_link,
+                time_duration: time_duration || recetteCourante.time_duration,
+                note: note || recetteCourante.note,
+                posting_date: posting_date || recetteCourante.posting_date,
+            };
+    
+            // Effectuez la mise à jour en utilisant les valeurs fusionnées
+            const resultat = await client.query(
+                "UPDATE recette SET titre = $2, image_link = $3, time_duration = $4, note = $5, posting_date = $6 WHERE id = $1",
+                [recetteId, nouvelleRecette.titre, nouvelleRecette.image_link, nouvelleRecette.time_duration, nouvelleRecette.note, nouvelleRecette.posting_date]
+            );
+    
+            res.json({ message: "Mise à jour réussie" });
+        } catch (e) {
+            console.log("Erreur lors de la mise à jour", e);
+            res.status(500).json({ error: "Erreur lors de la mise à jour de la recette" });
+        }
+    });
 
     app.listen(PORT, () => {
         console.log(`server est sur le port: ${PORT}`);
